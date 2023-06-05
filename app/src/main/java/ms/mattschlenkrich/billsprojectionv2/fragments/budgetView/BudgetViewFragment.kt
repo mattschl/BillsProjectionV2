e67ca5cbf9eaea44bb4d51feb14e86b50c5239ef
Matt Schenkrich <matt_schl@hotmail.com>
2023-06-04 17:40:16 -0700

    added BudgetViewFragment

---
+++ app/src/main/java/ms/mattschlenkrich/billsprojectionv2/fragments/budgetView/BudgetViewFragment.kt
@@ -0,0 +1,24 @@
+package ms.mattschlenkrich.billsprojectionv2.fragments.budgetView
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ms.mattschlenkrich.billsprojectionv2.R
+
+class BudgetViewFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_budget_view, container, false)
+    }
+}
--- app/src/main/java/ms/mattschlenkrich/billsprojectionv2/fragments/transactions/TransactionViewFragment.kt
+++ app/src/main/java/ms/mattschlenkrich/billsprojectionv2/fragments/transactions/TransactionViewFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ms.mattschlenkrich.billsprojectionv2.MainActivity
 import ms.mattschlenkrich.billsprojectionv2.R
+import ms.mattschlenkrich.billsprojectionv2.databinding.FragmentTransactionViewBinding
 
-class TransactionViewFragment : Fragment() {
+class TransactionViewFragment :
+    Fragment(R.layout.fragment_transaction_view) {
+
+    private var _binding: FragmentTransactionViewBinding? = null
+    private val binding get() = _binding!!
+    private var mView: View? = null
+    private lateinit var mainActivity: MainActivity
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +26,18 @@ class TransactionViewFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction_view, container, false)
+    ): View {
+        _binding = FragmentTransactionViewBinding.inflate(
+            inflater, container, false
+        )
+        mainActivity = (activity as MainActivity)
+        mView = binding.root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
