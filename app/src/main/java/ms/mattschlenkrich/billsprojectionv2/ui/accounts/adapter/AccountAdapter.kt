997d44f4f3db723c26242a6781ed7076b13d4dcf
Matt Schenkrich <matt_schl@hotmail.com>
2024-12-26 16:03:50 -0800

    refactored AccountAdapter added string resources instead of hard coded strings

--- app/src/main/java/ms/mattschlenkrich/billsprojectionv2/ui/accounts/adapter/AccountAdapter.kt
+++ app/src/main/java/ms/mattschlenkrich/billsprojectionv2/ui/accounts/adapter/AccountAdapter.kt
@@ -1,7 +1,6 @@
 package ms.mattschlenkrich.billsprojectionv2.ui.accounts.adapter
 
 import android.app.AlertDialog
-import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +10,7 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import ms.mattschlenkrich.billsprojectionv2.R
 import ms.mattschlenkrich.billsprojectionv2.common.FRAG_ACCOUNTS
 import ms.mattschlenkrich.billsprojectionv2.common.FRAG_BUDGET_ITEM_ADD
 import ms.mattschlenkrich.billsprojectionv2.common.FRAG_BUDGET_ITEM_UPDATE
@@ -41,7 +41,7 @@ private const val PARENT_TAG = FRAG_ACCOUNTS
 class AccountAdapter(
     val mainActivity: MainActivity,
     private val mainViewModel: MainViewModel,
-    private val context: Context
+    private val mView: View,
 ) :
     RecyclerView.Adapter<AccountAdapter.AccountViewHolder>() {
 
@@ -127,33 +127,38 @@ class AccountAdapter(
             tvAccountName.text =
                 curAccount.account.accountName
             var info = if (curAccount.account.accountNumber.isNotEmpty()) {
-                "# ${curAccount.account.accountNumber}"
+                "# ${curAccount.account.accountNumber} "
             } else {
                 ""
             }
             if (curAccount.account.accountBalance != 0.0) {
-                info += "${if (info.isNotEmpty()) "\n" else ""}Balance " +
+                info += (if (info.isNotEmpty()) "\n" else "") +
+                        mView.context.getString(R.string.balance) +
                         cf.displayDollars(curAccount.account.accountBalance)
             }
             if (curAccount.account.accountOwing != 0.0) {
-                info += "${if (info.isNotEmpty()) "\n" else ""}Owing " +
+                info += (if (info.isNotEmpty()) "\n" else "") +
+                        mView.context.getString(R.string.owing) +
                         cf.displayDollars(curAccount.account.accountOwing)
             }
             if (curAccount.account.accBudgetedAmount != 0.0) {
-                info += "${if (info.isNotEmpty()) "\n" else ""}Budgeted " +
+                info += (if (info.isNotEmpty()) "\n" else "") +
+                        mView.context.getString(R.string.budgeted) +
                         cf.displayDollars(curAccount.account.accBudgetedAmount)
             }
             if (curAccount.account.accountCreditLimit != 0.0) {
-                info += "${if (info.isNotEmpty()) "\n" else ""}Credit Limit " +
+                info += (if (info.isNotEmpty()) "\n" else "") +
+                        mView.context.getString(R.string.credit_limit) +
                         cf.displayDollars(curAccount.account.accountCreditLimit)
             }
             if (curAccount.account.accIsDeleted) {
-                info += "${if (info.isNotEmpty()) "\n" else ""}       **Deleted** "
+                info += (if (info.isNotEmpty()) "\n" else "") +
+                        mView.context.getString(R.string.deleted)
             }
             if (info.isBlank()) {
-                info = "No info"
+                info = mView.context.getString(R.string.no_info)
             }
-            if (info == "No info") {
+            if (info == mView.context.getString(R.string.no_info)) {
                 tvAccountInfo.visibility = ViewGroup.GONE
             } else {
                 tvAccountInfo.visibility = ViewGroup.VISIBLE
@@ -171,56 +176,60 @@ class AccountAdapter(
             ibAccountColor.setBackgroundColor(color)
 
             holder.itemView.setOnClickListener {
-                chooseAccount(curAccount, it)
+                chooseAccount(curAccount)
             }
             holder.itemView.setOnLongClickListener {
-                chooseOptionsForAccount(curAccount, it)
+                chooseOptionsForAccount(curAccount)
                 false
             }
         }
     }
 
     private fun chooseOptionsForAccount(
-        curAccount: AccountWithType,
-        it: View
+        curAccount: AccountWithType
     ) {
         if (!mainViewModel.getCallingFragments()!!.contains(FRAG_TRANSACTION_ANALYSIS)) {
-            AlertDialog.Builder(context)
+            AlertDialog.Builder(mView.context)
                 .setTitle(
-                    "Choose an action for" +
+                    mView.context.getString(R.string.choose_an_action_for) +
                             curAccount.account.accountName
                 )
                 .setItems(
                     arrayOf(
-                        "Edit this Account",
-                        "Delete this Account",
-                        "View a summary of Transactions using this Account"
+                        mView.context.getString(R.string.edit_this_account),
+                        mView.context.getString(R.string.delete_this_account),
+                        mView.context.getString(R.string.view_a_summary_of_transactions_using_this_account)
                     )
                 ) { _, pos ->
                     when (pos) {
-                        0 -> gotoUpdateAccount(curAccount, it)
+                        0 -> gotoUpdateAccount(curAccount)
                         1 -> deleteAccount(curAccount)
-                        2 -> gotoAverage(curAccount, it)
+                        2 -> gotoTransactionAverage(curAccount)
                     }
 
                 }
-                .setNegativeButton("Cancel", null)
+                .setNegativeButton(mView.context.getString(R.string.cancel), null)
                 .show()
         } else {
             Toast.makeText(
-                context,
-                "Editing is not allowed right now",
+                mView.context,
+                mView.context.getString(R.string.editing_is_not_allowed_right_now),
                 Toast.LENGTH_LONG
             ).show()
         }
     }
 
+    private fun deleteAccount(curAccount: AccountWithType) {
+        accountViewModel.deleteAccount(
+            curAccount.account.accountId, df.getCurrentTimeAsString()
+        )
+    }
+
     private fun chooseAccount(
         curAccount: AccountWithType,
-        it: View
     ) {
         if (mainViewModel.getCallingFragments()!!.contains(FRAG_TRANSACTION_ANALYSIS)) {
-            gotoTransactionAverage(curAccount, it)
+            gotoTransactionAverage(curAccount)
         } else {
             when (mainViewModel.getRequestedAccount()) {
                 REQUEST_TO_ACCOUNT -> {
@@ -241,7 +250,7 @@ class AccountAdapter(
                     }
                     mBudgetItem.toAccount = curAccount.account
                     mainViewModel.setBudgetItem(mBudgetItem)
-                    gotoCallingFragment(it)
+                    gotoCallingFragment()
 
                 }
 
@@ -263,44 +272,22 @@ class AccountAdapter(
                     }
                     mBudgetItem.fromAccount = curAccount.account
                     mainViewModel.setBudgetItem(mBudgetItem)
-                    gotoCallingFragment(it)
+                    gotoCallingFragment()
 
                 }
             }
         }
     }
 
-    private fun gotoTransactionAverage(curAccount: AccountWithType, it: View) {
+    private fun gotoTransactionAverage(curAccount: AccountWithType) {
         mainViewModel.setCallingFragments(mainViewModel.getCallingFragments() + ", " + PARENT_TAG)
         mainViewModel.setAccountWithType(curAccount)
         mainViewModel.setBudgetRuleDetailed(null)
-        it.findNavController().navigate(
-            AccountsFragmentDirections
-                .actionAccountsFragmentToTransactionAverageFragment()
-        )
-    }
-
-    private fun gotoAverage(curAccount: AccountWithType, it: View) {
-        mainViewModel.setCallingFragments(
-            mainViewModel.getCallingFragments() + ", " + PARENT_TAG
-        )
-        mainViewModel.setBudgetRuleDetailed(null)
-        mainViewModel.setAccountWithType(curAccount)
-        it.findNavController().navigate(
-            AccountsFragmentDirections
-                .actionAccountsFragmentToTransactionAverageFragment()
-        )
-    }
-
-    private fun deleteAccount(curAccount: AccountWithType) {
-        accountViewModel.deleteAccount(
-            curAccount.account.accountId, df.getCurrentTimeAsString()
-        )
+        gotoTransactionAverageFragment()
     }
 
     private fun gotoUpdateAccount(
         curAccount: AccountWithType,
-        it: View
     ) {
         mainViewModel.setCallingFragments(
             mainViewModel.getCallingFragments() + ", " + PARENT_TAG
@@ -308,71 +295,117 @@ class AccountAdapter(
         mainViewModel.setAccountWithType(
             curAccount
         )
-        val direction = AccountsFragmentDirections
-            .actionAccountsFragmentToAccountUpdateFragment()
-        it.findNavController().navigate(direction)
+        gotoAccountUpdateFragment()
     }
 
-    private fun gotoCallingFragment(it: View) {
+    private fun gotoCallingFragment() {
         mainViewModel.setCallingFragments(
             mainViewModel.getCallingFragments()!!
                 .replace(", $FRAG_ACCOUNTS", "")
         )
-        if (mainViewModel.getCallingFragments()!!.contains(FRAG_TRANSACTION_SPLIT)) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToTransactionSplitFragment()
-            )
+        val callingFragment = mainViewModel.getCallingFragments()!!
+        if (callingFragment.contains(FRAG_TRANSACTION_SPLIT)) {
+            gotoTransactionSplitFragment()
         }
-        if (mainViewModel.getCallingFragments()!!
+        if (callingFragment
                 .contains(FRAG_BUDGET_RULE_ADD)
         ) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToBudgetRuleAddFragment()
-            )
-        } else if (mainViewModel.getCallingFragments()!!
+            gotoBudgetRuleAddFragment()
+        } else if (callingFragment
                 .contains(FRAG_BUDGET_RULE_UPDATE)
         ) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToBudgetRuleUpdateFragment()
-            )
-        } else if (mainViewModel.getCallingFragments()!!
+            gotoBudgetRuleUpdateFragment()
+        } else if (callingFragment
                 .contains(FRAG_TRANS_ADD)
         ) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToTransactionAddFragment()
-            )
-        } else if (mainViewModel.getCallingFragments()!!
+            gotoTransactionAddFragment()
+        } else if (callingFragment
                 .contains(FRAG_TRANS_UPDATE)
         ) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToTransactionUpdateFragment()
-            )
-        } else if (mainViewModel.getCallingFragments()!!
+            gotoTransactionUpdateFragment()
+        } else if (callingFragment
                 .contains(FRAG_BUDGET_ITEM_ADD)
         ) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToBudgetItemAddFragment()
-            )
-        } else if (mainViewModel.getCallingFragments()!!
+            gotoBudgetItemAddFragment()
+        } else if (callingFragment
                 .contains(FRAG_BUDGET_ITEM_UPDATE)
         ) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToBudgetItemUpdateFragment()
-            )
-        } else if (mainViewModel.getCallingFragments()!!
+            gotoBudgetItemUpdateFragment()
+        } else if (callingFragment
                 .contains(FRAG_TRANS_PERFORM)
         ) {
-            it.findNavController().navigate(
-                AccountsFragmentDirections
-                    .actionAccountsFragmentToTransactionPerformFragment()
-            )
+            gotoTransactionPerformFragment()
         }
+    }
+
+    private fun gotoTransactionAverageFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToTransactionAverageFragment()
+        )
+    }
+
+    private fun gotoAccountUpdateFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToAccountUpdateFragment()
+        )
+    }
+
+
+    private fun gotoTransactionPerformFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToTransactionPerformFragment()
+        )
+    }
+
+    private fun gotoTransactionSplitFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToTransactionSplitFragment()
+        )
+    }
+
+    private fun gotoBudgetItemUpdateFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToBudgetItemUpdateFragment()
+        )
+    }
+
+    private fun gotoBudgetItemAddFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToBudgetItemAddFragment()
+        )
+    }
+
+    private fun gotoTransactionUpdateFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToTransactionUpdateFragment()
+        )
+    }
+
+    private fun gotoTransactionAddFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToTransactionAddFragment()
+        )
+    }
+
+    private fun gotoBudgetRuleUpdateFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToBudgetRuleUpdateFragment()
+        )
+    }
+
+    private fun gotoBudgetRuleAddFragment() {
+        mView.findNavController().navigate(
+            AccountsFragmentDirections
+                .actionAccountsFragmentToBudgetRuleAddFragment()
+        )
     }
 }
