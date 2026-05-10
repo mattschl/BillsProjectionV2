157c74b974353f6e7d83e5e1d90a63f35717af60
Matt Schenkrich <matt_schl@hotmail.com>
2026-05-10 16:14:22 -0700

    dark mode enabled - small error

--- app/src/main/java/ms/mattschlenkrich/billsprojectionv2/ui/theme/Theme.kt
+++ app/src/main/java/ms/mattschlenkrich/billsprojectionv2/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package ms.mattschlenkrich.billsprojectionv2.ui.theme
 
 import android.os.Build
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -33,6 +34,7 @@ fun BillsProjectionTheme(
         val fontSize = try {
             settings?.fontSize
         } catch (e: Exception) {
+            Log.d("BillsProjectionTheme", "Using default font size", e)
             "normal"
         }
         when (fontSize) {
